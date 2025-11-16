5be87f59745451dd2eb1ba793b017693380c9211
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2025-11-16 22:10:12 +0900

    feat : EnemyProtoType

--- Assets/01.Member/KMJ/02.Scripts/Enemy/EnemyAI/Enemy.cs
+++ Assets/01.Member/KMJ/02.Scripts/Enemy/EnemyAI/Enemy.cs
@@ -2,14 +2,21 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    
+    public bool isDead = false;
+    private bool _isMyTurn = false;
     public void Die()
     {
-        
+        gameObject.SetActive(false);
     }
     
     public void Hit()
     {
-        
+        if (isDead)
+            return;
+    }
+
+    public bool IsMyTurn()
+    {
+        return _isMyTurn;
     }
 }

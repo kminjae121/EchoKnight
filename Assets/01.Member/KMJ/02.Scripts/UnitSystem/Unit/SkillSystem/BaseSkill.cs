4d40a049a76d9f6cc85d5bc6ba3a1649a0327d43
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2025-12-18 23:17:46 +0900

    feat : BaseSkill

--- Assets/01.Member/KMJ/02.Scripts/UnitSystem/Unit/SkillSystem/BaseSkill.cs
+++ Assets/01.Member/KMJ/02.Scripts/UnitSystem/Unit/SkillSystem/BaseSkill.cs
@@ -4,14 +4,31 @@ namespace Code.UnitSystem.SkillSystem
 {
     public abstract class BaseSkill : MonoBehaviour
     {
+        [SerializeField] protected SkillComponent _skillCompo;
+        
         public float damage;
 
         public int useSkillPoint;
 
-        public void UseSkill()
+        public bool isCanUseSkill = false;
+        public virtual void UseSkill()
         {
+            if (_skillCompo.currentSkillCost - useSkillPoint < 0)
+                return;
             
+            _skillCompo.currentSkillCost -= useSkillPoint;
         }
+
+        public void CanUseThisSkill()
+        {
+            isCanUseSkill = true;
+        }
+        
+        public void BlockThisSkill()
+        {
+            isCanUseSkill = false;
+        }
+        
         
     }
 }

4199345d7af8df1e37a40d1a4ff2f7aad8d3224f
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2025-12-21 00:16:02 +0900

    feat : AnimationAllStop

--- Assets/01.Member/KMJ/02.Scripts/UnitSystem/Unit/UnitComponent/UnitAnimation.cs
+++ Assets/01.Member/KMJ/02.Scripts/UnitSystem/Unit/UnitComponent/UnitAnimation.cs
@@ -5,9 +5,40 @@ namespace UnitSystem
 {
     public class UnitAnimation : MonoBehaviour, IUnitComponent
     {
+        [SerializeField] private Animator _animator;
         public void Initialize(Unit owner)
         {
+            AnimationAllStop();
+        }
+
+        public void PlaySelectAnimation(string animationName)
+        {
+            AnimationAllStop();
             
+            _animator.SetBool(animationName,true);
+        }
+
+        public void ReturnIdleAnimation()
+        {
+            AnimationAllStop();
+
+            _animator.SetBool("IDLE", true);
+        }
+
+        public void AnimationAllStop()
+        {
+            if (_animator == null) return;
+
+            var parameters = _animator.parameters;
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                var p = parameters[i];
+                
+                if (p.type == AnimatorControllerParameterType.Bool)
+                {
+                    _animator.SetBool(p.name, false);
+                }
+            }
         }
     }
 }

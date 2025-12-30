4b5f70f689be3d3f73a5e1200f78d883127d5803
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2025-12-30 18:21:00 +0900

    feat : HitStop

--- Assets/01.Member/KMJ/02.Scripts/UnitSystem/Unit/UnitComponent/UnitAttackComponent.cs
+++ Assets/01.Member/KMJ/02.Scripts/UnitSystem/Unit/UnitComponent/UnitAttackComponent.cs
@@ -211,7 +211,7 @@ namespace Code.UnitSystem
 
         public void TakeDamage()
         {
-            Bus<HitStopEvent>.Raise(new HitStopEvent(0.3f));
+            Bus<HitStopEvent>.Raise(new HitStopEvent(0.6f));
             impulseSource.GenerateImpulse(0.6f);  
             
             _targetEnemy.GetComponent<EntityHealth>().ApplyDamage(_damageData, 

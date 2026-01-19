938d0a713e5164eac2ea28fc76f182ca2e64fa63
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2026-01-19 22:46:09 +0900

    feat : SkillUI

---
+++ Assets/_Code/Core/EventBus/Events/SkillUIEvent.cs
@@ -0,0 +1,21 @@
+﻿using Code.UnitSystem.SkillSystem;
+using UnityEngine.UI;
+
+namespace Code.Core.Events.Bus
+{
+    public struct SkillUIEvent : IEvent
+    {
+        public SkillComponent skillComponent;
+        public int skillIdx;
+        public string skillName;
+        public Image skillImage;
+
+        public SkillUIEvent(int skillIdx, string skillName, Image skillImage, SkillComponent skillComponent)
+        {
+            this.skillIdx = skillIdx;
+            this.skillComponent = skillComponent;
+            this.skillName = skillName;
+            this.skillImage = skillImage;
+        }
+    }
+}

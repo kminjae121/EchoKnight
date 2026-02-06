63776b75bfc019b2f6248dabef48a0d174423bff
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2026-02-06 22:52:19 +0900

    feat : UnitStatCompo

---
+++ Assets/_Code/KMJ/UnitSystem/Unit/UnitComponent/UnitStatCompo.cs
@@ -0,0 +1,44 @@
+﻿using Code.UnitSystem;
+using EntityComponent;
+using UnityEngine;
+
+namespace _Code.KMJ.UnitSystem.Unit.UnitComponent
+{
+    public enum StatInfo
+    {
+        MoveSpeed,
+        AtkDamage,
+        MaxHealth,
+    }
+    public class UnitStatCompo : MonoBehaviour, IUnitComponent
+    {
+        private UnitSO unitSO;
+
+        public float MoveSpeed => unitSO.MoveSpeed;
+
+        public float AtkDamage => unitSO.AtkDamage;
+
+        public float MaxHealth => unitSO.Maxhealth;
+
+        public void Initialize(Code.UnitSystem.Unit owner)
+        {
+            unitSO = owner.unitSO;
+        }
+        
+        public T GetStat<T>(StatInfo statInfo)
+        {
+            object value = statInfo switch
+            {
+                StatInfo.MoveSpeed => MoveSpeed,
+                StatInfo.AtkDamage => AtkDamage,
+                StatInfo.MaxHealth => MaxHealth,
+                _ => default(T)
+            };
+            
+            if (value is T typedValue)
+                return typedValue;
+
+            return default;
+        }
+    }
+}

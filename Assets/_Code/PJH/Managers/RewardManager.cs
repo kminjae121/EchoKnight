1cc41c152539b7a73c57c80b9f8836842808b53e
jiho08 <57872222k@gmail.com>
2026-02-26 20:37:38 +0900

    feat : reward ui, item so

---
+++ Assets/_Code/PJH/Managers/RewardManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Code.Core.Events.Bus;
+using Code.Items;
+using Code.UI;
+using UnityEngine;
+
+namespace Code.Managers
+{
+    public class RewardManager : MonoBehaviour
+    {
+        [SerializeField] private List<ItemSO> itemList;
+        [SerializeField] BattleRewardUI battleRewardUI;
+        
+        private void Awake()
+        {
+            Bus<StageClearEvent>.Subscribe(HandleStageClear);
+        }
+
+        private void OnDestroy()
+        {
+            Bus<StageClearEvent>.Unsubscribe(HandleStageClear);
+        }
+
+        private void HandleStageClear(StageClearEvent evt)
+        {
+            List<ItemSO> rewardItems = new();
+            
+            // 임시
+            rewardItems.Add(itemList[Random.Range(0, itemList.Count)]);
+            rewardItems.Add(itemList[Random.Range(0, itemList.Count)]);
+            
+            battleRewardUI.Open(rewardItems);
+        }
+    }
+}

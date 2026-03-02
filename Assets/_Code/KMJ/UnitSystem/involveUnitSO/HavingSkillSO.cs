afbc067cabe9dac6a9b1293497af756e762be8a6
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2026-03-02 16:57:33 +0900

    feat : SkillAddSystem

---
+++ Assets/_Code/KMJ/UnitSystem/involveUnitSO/HavingSkillSO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Code.UnitSystem.SkillSystem;
+using UnityEngine;
+
+namespace _Code.KMJ.UnitSystem.involveUnitSO
+{
+    [CreateAssetMenu(fileName = "HavingSkillSO", menuName = "SkillSO", order = 0)]
+    public class HavingSkillSO : ScriptableObject
+    {
+        public List<SkillSO> HaveSkills = new List<SkillSO>();
+    }
+}

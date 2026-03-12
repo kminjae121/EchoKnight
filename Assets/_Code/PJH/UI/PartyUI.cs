1e1da12e8bdf1cfe234db727c5eab595126db83a
kimdonghyun81222 <157779631+kimdonghyun81222@users.noreply.github.com>
2026-03-12 18:53:18 +0900

    fixed :

--- Assets/_Code/PJH/UI/PartyUI.cs
+++ Assets/_Code/PJH/UI/PartyUI.cs
@@ -1,47 +1,82 @@
 ﻿using System.Collections.Generic;
+using _00.Core._02.Scripts._01.Manager;
+using Code.Core.Debugs;
 using Code.Core.Events.Bus;
-using GondrLib.Dependencies;
-using GondrLib.ObjectPool.Runtime;
+using Code.UnitSystem;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Code.UI
 {
     public class PartyUI : MonoBehaviour
     {
-        [Header("Pool Settings")]
-        [SerializeField] private PoolingItemSO partySlotPoolingSO;
-        
-        [Header("Containers")]
-        [SerializeField] private Transform slotContainer;
+        [Header("Buttons")]
+        [SerializeField] private Button startButton;
 
-        [Inject] private PoolManagerMono _poolManager;
-        
-        private List<PartyCharacterSlotUI> _activeSlots = new();
+        [Header("Slots")]
+        [SerializeField] private List<SelectedCharacterSlotUI> characterSlots;
+
+        [Header("Data")]
+        [SerializeField] private UnitStorageSO unitStorage;
+        [SerializeField] private int maxUnitCount = 3;
+
+        private readonly List<UnitSO> _partyUnits = new();
 
         private void Awake()
         {
-            if (_poolManager == null)
-                _poolManager = FindFirstObjectByType<PoolManagerMono>();
+            Bus<PartyCharacterSelectEvent>.Subscribe(HandleCharacterSelected);
+            Bus<PartyCharacterDeselectEvent>.Subscribe(HandleCharacterDeselected);
+
+            startButton.onClick.AddListener(HandleStartButton);
         }
 
-        private void OnEnable()
+        private void OnDestroy()
         {
-            Bus<PartyCharacterSelectEvent>.Subscribe(HandlePartySelectEvent);
+            Bus<PartyCharacterSelectEvent>.Unsubscribe(HandleCharacterSelected);
+            Bus<PartyCharacterDeselectEvent>.Unsubscribe(HandleCharacterDeselected);
+
+            startButton.onClick.RemoveListener(HandleStartButton);
         }
 
-        private void OnDisable()
+        private void HandleCharacterSelected(PartyCharacterSelectEvent evt)
         {
-            Bus<PartyCharacterSelectEvent>.Unsubscribe(HandlePartySelectEvent);
-        }
-        
-        private void HandlePartySelectEvent(PartyCharacterSelectEvent evt)
-        {
-            RefreshPartyList();
+            if (_partyUnits.Count >= maxUnitCount || _partyUnits.Contains(evt.Unit))
+                return;
+
+            _partyUnits.Add(evt.Unit);
+            RefreshSlots();
         }
 
-        private void RefreshPartyList()
+        private void HandleCharacterDeselected(PartyCharacterDeselectEvent evt)
         {
-            _activeSlots.Clear();
+            if (_partyUnits.Remove(evt.Unit))
+                RefreshSlots();
+        }
+
+        private void RefreshSlots()
+        {
+            for (int i = 0; i < characterSlots.Count; ++i)
+                characterSlots[i].SetUnit(i < _partyUnits.Count ? _partyUnits[i] : null);
+        }
+
+        private void HandleStartButton()
+        {
+            if (_partyUnits.Count == 0)
+            {
+                UnityLogger.Log("파티에 유닛이 없습니다.");
+                return;
+            }
+
+            unitStorage.units.Clear();
+            unitStorage.unitStates.Clear();
+
+            foreach (var unit in _partyUnits)
+            {
+                unitStorage.units.Add(unit.UnitSpawn);
+                unitStorage.unitStates.Add(new UnitState(unit));
+            }
+
+            SceneChangeManager.Instance.ChangeSelectScene("ExpeditionMapScene");
         }
     }
 }

f59ef51c105b3d1f30f80133e7e1e0d3dfafb80a
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2026-03-23 23:25:10 +0900

    feat : PrefabError

--- Assets/_Code/KMJ/UI/SkillTreeUI/StatNode.cs
+++ Assets/_Code/KMJ/UI/SkillTreeUI/StatNode.cs
@@ -1,4 +1,6 @@
 ﻿using _Code.KMJ.UnitSystem;
+using Code.Core.Managers;
+using Code.Managers;
 using Code.UnitManaging;
 using Code.UnitSystem;
 using UnityEngine;
@@ -11,8 +13,15 @@ namespace Code.UI.SkillTreeUI
         [SerializeField] private StatInfo upgradeStat;
         [SerializeField] private float upgradeValue;
         
+        
+        [SerializeField] private int nodePrice;
+        
         public void UseNode()
         {
+            if (nodePrice > PlayerManager.Instance.Gold)
+                return;
+            
+            PlayerManager.Instance.RemoveGold(nodePrice);
             InGameStatCompo.Instance.SetStat(upgradeStat, upgradeValue, unitType);
         }
     }

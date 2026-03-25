c3339fa95959871baa012316a9acc1d61ef5e978
jiho08 <57872222k@gmail.com>
2026-03-26 00:29:08 +0900

    fix : skill component code

--- Assets/_Code/KMJ/UnitSystem/Unit/SkillSystem/SkillComponent.cs
+++ Assets/_Code/KMJ/UnitSystem/Unit/SkillSystem/SkillComponent.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Code.Core.Events.Bus;
 using Code.Core.Managers;
-using Code.UnitSystem.Combat;
 using UnityEngine;
 
 namespace Code.UnitSystem.SkillSystem
@@ -26,46 +25,40 @@ namespace Code.UnitSystem.SkillSystem
             
             if (_unit != null && _statCompo == null)
                 _statCompo = _unit.GetUnitCompo<UnitStatCompo>();
-            
-            SkillSendManager.Instance.GetEquipSkills(_unit.unitSO.UnitType).ToList().ForEach(skill =>
-            {
+
+            foreach (var skill in SkillSendManager.Instance.GetEquipSkills(_unit.unitSO.UnitType))
                 _skillList.Add(skill);
-            });
             
             skills = new Dictionary<SkillSO, BaseSkill>();
             
 
-            foreach (var skillSo in _skillList)
+            foreach (var skillData in _skillList)
             {
-                if (skillSo == null || string.IsNullOrEmpty(skillSo.className))
+                if (skillData == null || string.IsNullOrEmpty(skillData.className))
                     continue;
 
-                Type type = GetTypeByName(skillSo.className);
+                Type type = GetTypeByName(skillData.className);
 
                 if (type == null)
                 {
-                    Debug.LogError(
-                        $"[SkillComponent] '{_unit.name}'의 스킬 '{skillSo.skillName}'에 해당하는 클래스 '{skillSo.className}'를 찾을 수 없습니다. (네임스페이스 확인 필요)");
+                    Debug.LogError($"[SkillComponent] '{_unit.name}'의 스킬 '{skillData.skillName}'에 해당하는 클래스 '{skillData.className}'를 찾을 수 없습니다. (네임스페이스 확인 필요)");
                     continue;
                 }
 
-                var components = _unit.GetComponentsInChildren(type, true);
+                var component = _unit.GetComponentInChildren(type, true);
 
-                if (components.Length > 0)
+                if (component is BaseSkill baseSkill)
                 {
-                    BaseSkill component = components[0] as BaseSkill;
-
-                    if (component != null)
-                    {
-                        component.UseSkillPoint = skillSo.UsingSkillCost;
-                        skills.TryAdd(skillSo, component);
-                    }
+                    if (component == null)
+                        continue;
+                    
+                    baseSkill.UseSkillPoint = skillData.UsingSkillCost;
+                    skills.TryAdd(skillData, baseSkill);
                 }
                 else
                     Debug.LogWarning($"[SkillComponent] '{_unit.name}'에 스킬 컴포넌트 '{type.Name}'가 부착되어 있지 않습니다.");
             }
             
-
             if (skills.Count > 0)
                 foreach (var skill in skills.Values)
                 {
@@ -74,8 +67,8 @@ namespace Code.UnitSystem.SkillSystem
                         if (_statCompo != null)
                         {
                             float skillDamageValue = _statCompo.GetStat(StatInfo.SkillDamage);
-                            float floatdamage = skill.basicSkillDamage * skillDamageValue;
-                            basicDamage = (int)floatdamage;
+                            float floatDamage = skill.basicSkillDamage * skillDamageValue;
+                            basicDamage = (int)floatDamage;
                         }
                         else
                             basicDamage = skill.basicSkillDamage;
@@ -85,8 +78,8 @@ namespace Code.UnitSystem.SkillSystem
                         if (_statCompo != null)
                         {
                             float atkDamage = _statCompo.GetStat(StatInfo.AtkDamage);
-                            float floatdamage = atkDamage;
-                            basicDamage = (int)floatdamage;
+                            float floatDamage = atkDamage;
+                            basicDamage = (int)floatDamage;
                         }
                         else
                             basicDamage = skill.basicSkillDamage;
@@ -104,7 +97,6 @@ namespace Code.UnitSystem.SkillSystem
             Bus<UsingSkillEvent>.Unsubscribe(BooleanSkill);
         }
         
-
         private Type GetTypeByName(string className)
         {
             Type type = Type.GetType(className);
@@ -123,8 +115,7 @@ namespace Code.UnitSystem.SkillSystem
 
             return null;
         }
-
-
+        
         private void BooleanSkill(UsingSkillEvent evt)
         {
             isUseSkill = evt.isUsingSkill;
@@ -138,29 +129,28 @@ namespace Code.UnitSystem.SkillSystem
         public void SetAddSkillDamage(float addDamage,SkillType skillType)
         {
             foreach (var skill in skills.Values)
-            {
                 if (skill.SkillType == skillType)
                 {
                     float damage = basicDamage + addDamage;
                     skill.SetDamage(damage);
                 }
-            }
         }
+        
         public void StartSkill(SkillSO skillSO)
         {
-            if (isUseSkill)
+            if (!isUseSkill)
+                return;
+            
+            if (!skills.ContainsKey(skillSO))
+                return;
+
+            BaseSkill skill = skills.GetValueOrDefault(skillSO);
+
+            if (skill != null)
             {
-                if (!skills.ContainsKey(skillSO))
-                    return;
-
-                BaseSkill skill = skills.GetValueOrDefault(skillSO);
-
-                if (skill != null)
-                {
-                    skill.ConfigureSkillRange(skillSO);
-                    skill.ShowSkillRange();
-                    Bus<UsingSkillEvent>.Raise(new UsingSkillEvent(false));
-                }
+                skill.ConfigureSkillRange(skillSO);
+                skill.ShowSkillRange();
+                Bus<UsingSkillEvent>.Raise(new UsingSkillEvent(false));
             }
         }
 

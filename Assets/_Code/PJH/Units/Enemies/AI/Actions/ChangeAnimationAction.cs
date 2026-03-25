f0cf2864d7cca9e487337035be543858dbb75332
jiho08 <57872222k@gmail.com>
2026-03-25 12:40:07 +0900

    feat : animation channel, action, enemy pooling

---
+++ Assets/_Code/PJH/Units/Enemies/AI/Actions/ChangeAnimationAction.cs
@@ -0,0 +1,22 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+namespace Code.UnitSystem.Enemies.AI
+{
+    [Serializable, GeneratePropertyBag]
+    [NodeDescription(name: "ChangeAnimation", story: "[UnitAnimator] change to [animName]", category: "Action", id: "fb10095125fc8a3f972d297b55a57e1b")]
+    public partial class ChangeAnimationAction : Action
+    {
+        [SerializeReference] public BlackboardVariable<UnitAnimation> UnitAnimator;
+        [SerializeReference] public BlackboardVariable<string> AnimName;
+
+        protected override Status OnStart()
+        {
+            UnitAnimator.Value.PlaySelectAnimation(AnimName.Value);
+            return Status.Success;
+        }
+    }
+}

a674becca01313fb4bfbc0494a242c1711f18de0
jiho08 <57872222k@gmail.com>
2026-03-30 16:12:38 +0900

    fix : enemy

--- Assets/_Code/PJH/SkillSystem/MeleeEnemyAttack.cs
+++ Assets/_Code/PJH/SkillSystem/MeleeEnemyAttack.cs
@@ -1,4 +1,4 @@
-﻿using Code.Core.Debugs;
+using Code.Core.Debugs;
 using Code.Core.Events.Bus;
 using UnityEngine;
 
@@ -7,7 +7,7 @@ namespace Code.SkillSystem
     public class MeleeEnemyAttack : BaseSkill
     {
         private GameObject _target;
-        
+
         protected void Start()
         {
             SkillEvent.AddListener(AttackAction);
@@ -23,11 +23,13 @@ namespace Code.SkillSystem
         protected override void OnDestroy()
         {
             SkillEvent.RemoveListener(AttackAction);
+
             if (triggerCompo != null)
             {
                 triggerCompo.OnAttackTrigger -= TakeDamage;
                 triggerCompo.OnAnimationEndTrigger -= SkillEnd;
             }
+
             base.OnDestroy();
         }
 
@@ -35,14 +37,13 @@ namespace Code.SkillSystem
         {
             _target = target;
         }
-        
+
         private void TakeDamage()
         {
-            // 임펄스랑 데미지
             UnityLogger.Log("적이 데미지를 주었습니다.");
-            Bus<DamageEvent>.Raise(new DamageEvent(DamageData, attackData, _target, AddDamage,null , false));
+            Bus<DamageEvent>.Raise(new DamageEvent(DamageData, attackData, _target, AddDamage, null, false));
         }
-        
+
         private void SkillEnd()
         {
             triggerCompo.OnAttackTrigger -= TakeDamage;

167720a208205ef83e238a7242e90c6022d5b64f
jiho08 <57872222k@gmail.com>
2026-03-30 23:03:16 +0900

    feat : status effect

---
+++ Assets/_Code/PJH/Combat/StatusEffect/EffectType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Code.Combat.StatusEffect
+{
+    [Flags]
+    public enum EffectType
+    {
+        None = 0,
+        Poison = 1 << 1,
+        Stunned = 1 << 2
+    }
+}

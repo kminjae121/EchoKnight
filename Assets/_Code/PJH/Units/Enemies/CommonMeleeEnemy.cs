f6177b20c1238c36b7842e0aad6cbe69b183a8a0
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2026-04-02 09:08:23 +0900

    feat : enemy

--- Assets/_Code/PJH/Units/Enemies/CommonMeleeEnemy.cs
+++ Assets/_Code/PJH/Units/Enemies/CommonMeleeEnemy.cs
@@ -1,6 +1,12 @@
+using UnityEngine;
+
 namespace Code.UnitSystem.Enemies
 {
     public class CommonMeleeEnemy : AbstractEnemyUnit
     {
+        protected override void Dead()
+        {
+            base.Dead();
+        }
     }
 }

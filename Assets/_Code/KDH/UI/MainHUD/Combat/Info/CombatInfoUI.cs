0f270f03a9b9977189697a9c9887b032889ea66d
kimdonghyun81222 <157779631+kimdonghyun81222@users.noreply.github.com>
2026-04-08 23:28:08 +0900

    feat : CombatProfileUI, fixed: CombatInfoUI

--- Assets/_Code/KDH/UI/MainHUD/Combat/Info/CombatInfoUI.cs
+++ Assets/_Code/KDH/UI/MainHUD/Combat/Info/CombatInfoUI.cs
@@ -28,6 +28,10 @@ namespace Code.UI
         [SerializeField] private Image unitImage;
         [SerializeField] private TextMeshProUGUI unitNameText;
         [SerializeField] private TextMeshProUGUI unitClassText;
+        
+        [Header("Health Info")]
+        [SerializeField] private TextMeshProUGUI hpText;
+        [SerializeField] private Image hpFillImage;
 
         [Header("Stats")]
         [SerializeField] private TextMeshProUGUI turnSpeedText;
@@ -44,6 +48,8 @@ namespace Code.UI
         [SerializeField] private RectTransform skillPanelGroup;
         [SerializeField] private PoolingItemSO characterSkillButtonPoolingSO;
         [SerializeField] private List<Image> artifactIcons;
+        [SerializeField] private List<Image> artifactRarityImages;
+        [SerializeField] private List<Sprite> raritySprites;
         [SerializeField] private Sprite emptyArtifactSprite;
 
         private Tween _slideTween;
@@ -61,73 +67,68 @@ namespace Code.UI
                 Debug.LogError("[CombatInfoUI] 풀 매니저를 찾을 수 없습니다.");
             }
 
-            if (panelRect == null)
-            {
-                panelRect = GetComponent<RectTransform>();
-            }
-            
+            if (panelRect == null) panelRect = GetComponent<RectTransform>();
             panelRect.anchoredPosition = hiddenPosition;
             
-            if (backgroundPanel != null)
-            {
-                backgroundPanel.SetActive(false);
-            }
-
-            if (openButton != null)
-            {
-                openButton.onClick.AddListener(ShowUI);
-            }
-
-            if (closeButton != null)
-            {
-                closeButton.onClick.AddListener(HideUI);
-            }
+            if (backgroundPanel != null) backgroundPanel.SetActive(false);
+            if (openButton != null) openButton.onClick.AddListener(ShowUI);
+            if (closeButton != null) closeButton.onClick.AddListener(HideUI);
 
             Bus<ShowCombatInfoEvent>.Subscribe(HandleShowCombatInfo);
-
             SetupArtifactTriggers();
         }
 
         private void OnDestroy()
         {
-            if (openButton != null)
-            {
-                openButton.onClick.RemoveListener(ShowUI);
-            }
-
-            if (closeButton != null)
-            {
-                closeButton.onClick.RemoveListener(HideUI);
-            }
+            if (openButton != null) openButton.onClick.RemoveListener(ShowUI);
+            if (closeButton != null) closeButton.onClick.RemoveListener(HideUI);
             
             Bus<ShowCombatInfoEvent>.Unsubscribe(HandleShowCombatInfo);
+            UnsubscribeHealth();
             _slideTween?.Kill();
         }
 
         private void HandleShowCombatInfo(ShowCombatInfoEvent evt)
         {
+            UnsubscribeHealth();
+
             if (evt.IsShow && evt.TargetUnit != null)
             {
                 _currentUnit = evt.TargetUnit;
+                
+                if (_currentUnit.CurrentHp != null)
+                {
+                    _currentUnit.CurrentHp.OnValueChanged += OnHealthChanged;
+                }
+                
                 RefreshAllUI();
                 ShowUI();
             }
             else
             {
                 HideUI();
+                _currentUnit = null;
             }
+        }
+
+        private void UnsubscribeHealth()
+        {
+            if (_currentUnit != null && _currentUnit.CurrentHp != null)
+            {
+                _currentUnit.CurrentHp.OnValueChanged -= OnHealthChanged;
+            }
+        }
+
+        private void OnHealthChanged(float prevHp, float nextHp)
+        {
+            RefreshHealthUI();
         }
 
         public void ShowUI()
         {
             if (_isVisible) return;
-            
             _isVisible = true;
-            
-            if (backgroundPanel != null)
-            {
-                backgroundPanel.SetActive(true);
-            }
+            if (backgroundPanel != null) backgroundPanel.SetActive(true);
 
             _slideTween?.Kill();
             _slideTween = panelRect.DOAnchorPos(visiblePosition, slideDuration).SetEase(slideEase);
@@ -136,16 +137,12 @@ namespace Code.UI
         public void HideUI()
         {
             if (!_isVisible) return;
-            
             _isVisible = false;
             
             _slideTween?.Kill();
             _slideTween = panelRect.DOAnchorPos(hiddenPosition, slideDuration).SetEase(Ease.InBack).OnComplete(() => 
             {
-                if (backgroundPanel != null)
-                {
-                    backgroundPanel.SetActive(false);
-                }
+                if (backgroundPanel != null) backgroundPanel.SetActive(false);
             });
             
             Bus<SkillUIHoverEvent>.Raise(new SkillUIHoverEvent(null, null));
@@ -161,15 +158,15 @@ namespace Code.UI
             if (unitImage != null) unitImage.sprite = data.UnitImage;
             if (unitNameText != null) unitNameText.text = data.UnitName;
             if (unitClassText != null) unitClassText.text = data.UnitClass;
+            
+            RefreshHealthUI();
 
             if (turnSpeedText != null) turnSpeedText.text = data.turnSpeed.ToString("F1");
             if (moveSpeedText != null) moveSpeedText.text = data.MoveRange.ToString("F1");
             if (maxHealthText != null) maxHealthText.text = data.Maxhealth.ToString("F1");
             if (attackDamageText != null) attackDamageText.text = data.AttackDamage.ToString("F1");
             if (defensivePowerText != null) defensivePowerText.text = data.DefensivePower.ToString("F1");
-            
             if (criticalProbabilityText != null) criticalProbabilityText.text = $"{data.CriticalProbability:F1}%";
-            
             if (criticalDamageIncreaseText != null) criticalDamageIncreaseText.text = data.CriticalDamageIncrease.ToString("F1");
             if (maxSkillCostText != null) maxSkillCostText.text = data.MaxSkillCost.ToString();
             if (recoverySkillCostText != null) recoverySkillCostText.text = data.RecoverySkillCost.ToString();
@@ -178,21 +175,27 @@ namespace Code.UI
             RefreshArtifacts();
         }
 
+        private void RefreshHealthUI()
+        {
+            if (_currentUnit != null && _currentUnit.Data != null)
+            {
+                float currentHp = _currentUnit.CurrentHp.Value;
+                float maxHp = _currentUnit.Data.Maxhealth;
+
+                if (hpText != null) hpText.text = $"{Mathf.CeilToInt(currentHp)} / {Mathf.CeilToInt(maxHp)}";
+                if (hpFillImage != null) hpFillImage.fillAmount = maxHp > 0 ? (currentHp / maxHp) : 0f;
+            }
+        }
+
         private void RefreshSkills()
         {
             SkillSO[] equippedSkills = System.Array.Empty<SkillSO>();
-            
             if (SkillSendManager.Instance != null && _currentUnit != null)
-            {
                 equippedSkills = SkillSendManager.Instance.GetEquipSkills(_currentUnit.Data.UnitType);
-            }
 
             foreach (var btn in _activeSkillButtons)
             {
-                if (btn != null)
-                {
-                    btn.ReturnToPool();
-                }
+                if (btn != null) btn.ReturnToPool();
             }
             _activeSkillButtons.Clear();
 
@@ -218,21 +221,45 @@ namespace Code.UI
         private void RefreshArtifacts()
         {
             var data = _currentUnit?.Data;
-            
             for (int i = 0; i < artifactIcons.Count; i++)
             {
-                bool hasArtifact = data != null && 
-                                   data.EquippedArtifacts != null && 
-                                   i < data.EquippedArtifacts.artifacts.Count && 
-                                   data.EquippedArtifacts.artifacts[i] != null;
-                                   
-                artifactIcons[i].sprite = hasArtifact ? data.EquippedArtifacts.artifacts[i].itemIcon : emptyArtifactSprite;
+                bool hasArtifact = data != null && data.EquippedArtifacts != null && 
+                                   i < data.EquippedArtifacts.artifacts.Count && data.EquippedArtifacts.artifacts[i] != null;
+
+                if (hasArtifact)
+                {
+                    var artifact = data.EquippedArtifacts.artifacts[i];
+                    artifactIcons[i].sprite = artifact.itemIcon;
+
+                    if (i < artifactRarityImages.Count && artifactRarityImages[i] != null)
+                    {
+                        if (artifact is EquipmentItemSO equipSO)
+                        {
+                            artifactRarityImages[i].gameObject.SetActive(true);
+                            
+                            int rarityIndex = (int)equipSO.rarity;
+                            if (raritySprites != null && rarityIndex >= 0 && rarityIndex < raritySprites.Count)
+                            {
+                                artifactRarityImages[i].sprite = raritySprites[rarityIndex];
+                            }
+                        }
+                        else
+                        {
+                            artifactRarityImages[i].gameObject.SetActive(false);
+                        }
+                    }
+                }
+                else
+                {
+                    artifactIcons[i].sprite = emptyArtifactSprite;
+                    if (i < artifactRarityImages.Count && artifactRarityImages[i] != null)
+                    {
+                        artifactRarityImages[i].gameObject.SetActive(false);
+                    }
+                }
                 
                 var trigger = artifactIcons[i].GetComponent<SlotHoverClickTrigger>();
-                if (trigger != null)
-                {
-                    trigger.SetInteractable(hasArtifact);
-                }
+                if (trigger != null) trigger.SetInteractable(hasArtifact);
             }
         }
 
@@ -242,11 +269,7 @@ namespace Code.UI
             {
                 int index = i;
                 var trigger = artifactIcons[i].GetComponent<SlotHoverClickTrigger>();
-                
-                if (trigger == null)
-                {
-                    trigger = artifactIcons[i].gameObject.AddComponent<SlotHoverClickTrigger>();
-                }
+                if (trigger == null) trigger = artifactIcons[i].gameObject.AddComponent<SlotHoverClickTrigger>();
                 
                 trigger.useHoverVisuals = false;
                 trigger.OnHoverEnter = (pivot, triggerOffset) =>
@@ -255,9 +278,7 @@ namespace Code.UI
                     {
                         var artifacts = _currentUnit.Data.EquippedArtifacts.artifacts;
                         if (index < artifacts.Count && artifacts[index] != null)
-                        {
                             Bus<CombatArtifactHoverEvent>.Raise(new CombatArtifactHoverEvent(artifacts[index], true));
-                        }
                     }
                 };
                 trigger.OnHoverExit = () => Bus<CombatArtifactHoverEvent>.Raise(new CombatArtifactHoverEvent(null, false));

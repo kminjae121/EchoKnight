059842e682799748f801ef45d2c3efcd4bef0974
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2026-04-08 13:52:07 +0900

    feat : PasiveSystem

--- Assets/_Code/KMJ/UnitSystem/Unit/Passive/PassiveCompo/ThornmailPassive.cs
+++ Assets/_Code/KMJ/UnitSystem/Unit/Passive/PassiveCompo/ThornmailPassive.cs
@@ -30,8 +30,12 @@ namespace _Code.Passive
         {
             DamageData damageData = new DamageData();
             damageData.damage = (int)(value * 0.3f);
-            target.GetComponent<IDamageable>().ApplyDamage(damageData, target.transform.position,
-                transform.transform.position, null, _character, false, false);
+
+            if (target.TryGetComponent<IDamageable>(out var damageable))
+            {
+                damageable.ApplyDamage(damageData, target.transform.position,
+                    transform.transform.position, null, _character, false, false);
+            }
         }
     }
 }

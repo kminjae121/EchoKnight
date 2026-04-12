e70531dc1112688bb70a7a790e397491a5e34324
kimdonghyun81222 <157779631+kimdonghyun81222@users.noreply.github.com>
2026-04-12 23:20:19 +0900

    feat: BillboardHpUI

---
+++ Assets/_Code/KDH/UI/Helper/BillboardHpUI.cs
@@ -0,0 +1,63 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Code.UnitSystem.Combat;
+
+namespace Code.UI
+{
+    public class BillboardHpUI : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private UnitHealth targetHealth;
+        [SerializeField] private Slider hpSlider;
+        [SerializeField] private TextMeshProUGUI hpText;
+
+        private Camera _mainCam;
+
+        private void Start()
+        {
+            _mainCam = Camera.main;
+
+            if (targetHealth == null)
+            {
+                targetHealth = GetComponentInParent<UnitHealth>();
+            }
+
+            if (targetHealth != null)
+            {
+                targetHealth.OnHealthChangedEvent += UpdateHpUI;
+                UpdateHpUI(targetHealth.CurrentHealth, targetHealth.MaxHealth);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (targetHealth != null)
+            {
+                targetHealth.OnHealthChangedEvent -= UpdateHpUI;
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if (_mainCam != null)
+            {
+                transform.rotation = _mainCam.transform.rotation;
+            }
+        }
+
+        private void UpdateHpUI(float currentHp, float maxHp)
+        {
+            if (hpSlider != null)
+            {
+                hpSlider.maxValue = maxHp;
+                hpSlider.value = currentHp;
+            }
+
+            if (hpText != null)
+            {
+                hpText.text = $"{Mathf.CeilToInt(currentHp)} / {Mathf.CeilToInt(maxHp)}";
+            }
+        }
+    }
+}

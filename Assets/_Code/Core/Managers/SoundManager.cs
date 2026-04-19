fb530766f87dcde7f7ca22366612fb9ba5ab0552
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2026-04-19 22:46:37 +0900

    feat : SoundManager
    사운드 매니저를 풀링 형식으로 직접 제작하고 범용성 있게 설계하고 있습니다.

--- Assets/_Code/Core/Managers/SoundManager.cs
+++ Assets/_Code/Core/Managers/SoundManager.cs
@@ -1,9 +1,62 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using _Code.KMJ.UnitSystem.Sound;
+using UnityEngine;
+using UnityEngine.Analytics;
 
 namespace Code.Core
 {
     public class SoundManager : MonoSingleton<SoundManager>
     {
+        [SerializeField] private List<SoundClip> clips;
+        [SerializeField] private Stack<AudioSource> audioSources;  
         
+        private Dictionary<string, AudioClip> _clipDictionary = new Dictionary<string, AudioClip>();
+        private Dictionary<string, AudioClip> _loopingClipDictionary = new Dictionary<string, AudioClip>();
+
+        protected override void Awake()
+        {
+            base.Awake();
+            
+            foreach (var audio in clips)
+            {
+                if (audio.IsLooping && !_loopingClipDictionary.ContainsKey(audio.AudioName))
+                {
+                    _loopingClipDictionary.Add(audio.AudioName, audio.Clip);
+                }
+                else if(!_clipDictionary.ContainsKey(audio.AudioName))
+                {
+                    _clipDictionary.Add(audio.AudioName, audio.Clip);
+                }
+            }
+        }
+
+
+        public void PlayLooping(string name)
+        {
+            _clipDictionary.TryGetValue(name, out AudioClip clip);
+
+            var _pool = audioSources.Pop();
+
+            _pool.gameObject.SetActive(true);
+            _pool.clip = clip;
+            _pool.loop = false;
+            
+            _pool.Play();
+        }
+
+        public void PlayClip(string name)
+        {
+            
+        }
+
+        public void StopClip(string name)
+        {
+            
+        }
+
+        public void StopLoopingClip(string name)
+        {
+            
+        }
     }
 }

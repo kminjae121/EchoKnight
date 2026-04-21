5340a5228478bbe699ed93dd8275428181cd99ab
kimdonghyun81222 <157779631+kimdonghyun81222@users.noreply.github.com>
2026-04-21 20:03:50 +0900

    fixed: ExpeditionManager

--- Assets/_Code/KDH/Expedition/ExpeditionManager.cs
+++ Assets/_Code/KDH/Expedition/ExpeditionManager.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿using Code.Expedition.Components;
+using UnityEngine;
+using Input; 
+using _00.Core._02.Scripts._01.Manager;
+using Code.Core;
+using Code.Core.Events.Bus;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using Newtonsoft.Json;
-using DG.Tweening;
-using Code.Core.Events.Bus;
+using UnityEngine.SceneManagement; 
 using Code.Expedition.Data;
-using _00.Core._02.Scripts._01.Manager;
 
 namespace Code.Expedition.Managers
 {
@@ -18,30 +18,28 @@ namespace Code.Expedition.Managers
         public GameObject uiPanel;
     }
 
-    [System.Serializable]
-    public struct NodeTypeSceneMapping
-    {
-        public NodeType nodeType;
-        public string sceneName;
-    }
-
     public class ExpeditionManager : MonoSingleton<ExpeditionManager>
     {
-        [Header("2D Map Generation Settings")]
-        public MapConfig config;
-        public MapView view;
-
-        [Header("Event & Scene Mapping")]
-        [SerializeField] private List<EventUIMapping> eventUIMappings;
-        [SerializeField] private List<NodeTypeSceneMapping> sceneMappings;
-
-        [Header("Tracker Settings")]
-        public bool lockAfterSelecting = false;
-        public float enterNodeDelay = 1f;
-
-        public Map.Map CurrentMap { get; private set; }
-        public bool Locked { get; set; }
+        [Header("References")]
+        [SerializeField] private ExpeditionPlayer player;
+        [SerializeField] private ExpeditionNode startNode;
+        [SerializeField] private InputReader inputReader;
+        [SerializeField] private LayerMask nodeLayer;
         
+        [Header("Camera")]
+        [SerializeField] private Camera mainCamera;
+
+        [Header("Event UIs")]
+        [SerializeField] private List<EventUIMapping> eventUIMappings; 
+
+        private ExpeditionNode _currentNode;
+        private ExpeditionNode _hoveredNode;
+        private ExpeditionNode _selectedNodeForMove; 
+        private bool _isMoving;
+
+        private static string _savedCurrentNodeName = "";
+        private static readonly HashSet<string> _savedClearedNodes = new();
+
         private Canvas _canvas;
 
         protected override void Awake()
@@ -50,177 +48,292 @@ namespace Code.Expedition.Managers
             DontDestroyOnLoad(gameObject);
         }
 
+        private void Start()
+        {
+            InitializeExpeditionScene();
+        }
+
+        private void Update()
+        {
+            HandleHover();
+        }
+
         private void OnEnable()
         {
+            if (inputReader != null)
+                inputReader.OnClickEvent += HandleClick;
+            
             Bus<StageClearEvent>.Subscribe(OnStageCleared);
-            SceneManager.sceneLoaded += OnSceneLoaded;
+            SceneManager.sceneLoaded += OnSceneLoaded; 
         }
 
         private void OnDisable()
         {
+            if (inputReader != null)
+                inputReader.OnClickEvent -= HandleClick;
+            
             Bus<StageClearEvent>.Unsubscribe(OnStageCleared);
-            SceneManager.sceneLoaded -= OnSceneLoaded;
-        }
-
-        private void Start()
-        {
-            InitializeMap();
+            SceneManager.sceneLoaded -= OnSceneLoaded; 
         }
 
         private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
         {
-            if (view != null && CurrentMap != null)
-            {
-                view.ShowMap(CurrentMap);
-                SetAttainableNodes();
-                SetLineColors();
-            }
+            InitializeExpeditionScene();
         }
 
-        private void InitializeMap()
+        private void InitializeExpeditionScene()
         {
-            if (PlayerPrefs.HasKey("ExpeditionMap"))
-            {
-                string mapJson = PlayerPrefs.GetString("ExpeditionMap");
-                Map.Map map = JsonConvert.DeserializeObject<Map.Map>(mapJson);
-                
-                if (map.path.Any(p => p.Equals(map.GetBossNode().point)))
-                {
-                    GenerateNewMap();
-                }
-                else
-                {
-                    CurrentMap = map;
-                    if (view != null) view.ShowMap(map);
-                }
-            }
-            else
-            {
-                GenerateNewMap();
-            }
-
-            Locked = false;
-        }
-
-        public void GenerateNewMap()
-        {
-            Map.Map map = MapGenerator.GetMap(config);
-            CurrentMap = map;
-            SaveMap();
-            if (view != null) view.ShowMap(map);
-        }
-
-        public void SaveMap()
-        {
-            if (CurrentMap == null) return;
-
-            string json = JsonConvert.SerializeObject(CurrentMap, Formatting.Indented,
-                new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
-            PlayerPrefs.SetString("ExpeditionMap", json);
-            PlayerPrefs.Save();
-        }
-
-        public void SelectNode(MapNode mapNode)
-        {
-            if (Locked) return;
-
-            if (CurrentMap.path.Count == 0)
-            {
-                if (mapNode.Node.point.y == 0)
-                    SendPlayerToNode(mapNode);
-                else
-                    Debug.LogWarning("첫 번째 층의 노드만 선택할 수 있습니다.");
-            }
-            else
-            {
-                Vector2Int currentPoint = CurrentMap.path.Last();
-                Node currentNode = CurrentMap.GetNode(currentPoint);
-
-                if (currentNode != null && currentNode.outgoing.Any(point => point.Equals(mapNode.Node.point)))
-                    SendPlayerToNode(mapNode);
-                else
-                    Debug.LogWarning("현재 위치에서 갈 수 없는 노드입니다.");
-            }
-        }
-
-        private void SendPlayerToNode(MapNode mapNode)
-        {
-            Locked = lockAfterSelecting;
-            CurrentMap.path.Add(mapNode.Node.point);
-            SaveMap();
+            ExpeditionNode[] allNodes = FindObjectsByType<ExpeditionNode>(FindObjectsSortMode.None);
             
-            SetAttainableNodes();
-            SetLineColors();
-            mapNode.ShowSwirlAnimation();
+            if (allNodes.Length == 0)
+                return;
 
-            DOTween.Sequence().AppendInterval(enterNodeDelay).OnComplete(() => EnterStage(mapNode));
-        }
+            if (mainCamera == null)
+                mainCamera = Camera.main;
+            
+            if (player == null)
+                player = FindAnyObjectByType<ExpeditionPlayer>();
 
-        private void EnterStage(MapNode mapNode)
-        {
-            NodeBlueprint blueprint = mapNode.Blueprint;
-            Debug.Log($"노드 진입: {blueprint.name} / 타입: {blueprint.nodeType}");
-
-            if (blueprint.nodeType == NodeType.Mystery || blueprint.nodeType == NodeType.Store || blueprint.nodeType == NodeType.RestSite)
+            if (string.IsNullOrEmpty(_savedCurrentNodeName))
             {
-                HandleEventNode(blueprint);
+                if (startNode != null)
+                {
+                    _currentNode = startNode;
+                    _savedCurrentNodeName = startNode.name; 
+                    _savedClearedNodes.Add(startNode.name); 
+                }
             }
             else
             {
-                HandleBattleNode(blueprint);
+                foreach (var node in allNodes)
+                    if (node.name == _savedCurrentNodeName)
+                    {
+                        _currentNode = node;
+                        break;
+                    }
+            }
+
+            foreach (var node in allNodes)
+                if (_savedClearedNodes.Contains(node.name))
+                    node.SetCleared(true);
+
+            if (_currentNode != null && player != null)
+                player.Initialize(_currentNode.transform.position);
+            
+            UpdateAllNodesVisuals(allNodes);
+
+            _hoveredNode = null;
+            _selectedNodeForMove = null;
+            _isMoving = false;
+        }
+
+        private void OnStageCleared(StageClearEvent evt)
+        {
+            if (evt.isClear)
+            {
+                if (!string.IsNullOrEmpty(_savedCurrentNodeName))
+                {
+                    _savedClearedNodes.Add(_savedCurrentNodeName);
+                    Debug.Log($"[{_savedCurrentNodeName}] 노드가 클리어 기록에 추가되었습니다!");
+                }
+
+                if (_currentNode != null)
+                {
+                    _currentNode.SetCleared(true);
+                    UpdateAllNodesVisuals(FindObjectsByType<ExpeditionNode>(FindObjectsSortMode.None));
+                }
             }
         }
 
-        private void HandleEventNode(NodeBlueprint blueprint)
+        private void UpdateAllNodesVisuals(ExpeditionNode[] allNodes)
         {
-            GameObject targetUIPrefab = null;
+            if (allNodes == null)
+                return;
+            
+            foreach (var node in allNodes)
+                node.UpdateMaterial(node == _currentNode);
+        }
 
-            if (_canvas == null)
+        private void HandleHover()
+        {
+            if (mainCamera == null || inputReader == null)
+                return;
+
+            Ray ray = mainCamera.ScreenPointToRay(inputReader.MousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, nodeLayer))
             {
-                Canvas[] canvases = FindObjectsByType<Canvas>(FindObjectsSortMode.None);
-                foreach (var canvas in canvases)
+                ExpeditionNode hitNode = hit.collider.GetComponent<ExpeditionNode>();
+                
+                if (hitNode != _hoveredNode)
                 {
-                    if (canvas.gameObject.name == "UI")
+                    if (_hoveredNode != null && _hoveredNode != _selectedNodeForMove) 
+                        _hoveredNode.SetOutline(false);
+
+                    _hoveredNode = hitNode;
+                    if (_hoveredNode != null) _hoveredNode.SetOutline(true);
+                }
+            }
+            else
+            {
+                if (_hoveredNode != null)
+                {
+                    if (_hoveredNode != _selectedNodeForMove)
+                        _hoveredNode.SetOutline(false);
+                    
+                    _hoveredNode = null;
+                }
+            }
+        }
+
+        private void HandleClick()
+        {
+            if (_isMoving) return;
+            if (mainCamera == null) return;
+            if (inputReader == null) return;
+
+            Vector2 mousePos = inputReader.MousePosition;
+            Ray ray = mainCamera.ScreenPointToRay(mousePos);
+            
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, nodeLayer))
+            {
+                ExpeditionNode hitNode = hit.collider.GetComponent<ExpeditionNode>();
+                
+                if (hitNode != null)
+                {
+                    if (_selectedNodeForMove == hitNode)
                     {
-                        _canvas = canvas;
-                        break;
+                        hitNode.SetOutline(false);
+                        hitNode.SetReadyToMoveColor(false);
+                        TryMoveToNode(hitNode);
+                        _selectedNodeForMove = null; 
+                    }
+                    else
+                    {
+                        if (_selectedNodeForMove != null)
+                        {
+                            if (_selectedNodeForMove != _hoveredNode)
+                                _selectedNodeForMove.SetOutline(false);
+                            
+                            _selectedNodeForMove.SetReadyToMoveColor(false);
+                        }
+
+                        _selectedNodeForMove = hitNode;
+                        _selectedNodeForMove.SetOutline(true); 
+                        _selectedNodeForMove.SetReadyToMoveColor(true);
+                        Debug.Log($"[{hitNode.name}] 노드가 선택되었습니다. 한 번 더 클릭하면 이동합니다.");
                     }
                 }
             }
-
-            if (eventUIMappings.Count > 0)
-            {
-                var mapping = eventUIMappings.FirstOrDefault(m => m.eventNodeData != null && m.eventNodeData.nodeName == blueprint.name);
-                targetUIPrefab = mapping.uiPanel != null ? mapping.uiPanel : eventUIMappings[0].uiPanel;
-            }
-
-            if (targetUIPrefab != null && _canvas != null)
-            {
-                GameObject uiInstance = Instantiate(targetUIPrefab, _canvas.transform);
-                uiInstance.transform.localPosition = new Vector3(13, -82, -12f);
-                uiInstance.SetActive(true);
-            }
             else
             {
-                Debug.LogWarning("이벤트 UI 프리팹 또는 Canvas 'UI'를 찾을 수 없습니다.");
+                if (_selectedNodeForMove != null)
+                {
+                    if (_selectedNodeForMove != _hoveredNode)
+                        _selectedNodeForMove.SetOutline(false);
+                    
+                    _selectedNodeForMove.SetReadyToMoveColor(false);
+                    _selectedNodeForMove = null;
+                    Debug.Log("노드 선택이 취소되었습니다.");
+                }
             }
-
-            Locked = false;
         }
 
-        private void HandleBattleNode(NodeBlueprint blueprint)
+        private void TryMoveToNode(ExpeditionNode targetNode)
         {
-            string targetSceneName = string.Empty;
-
-            var mapping = sceneMappings.FirstOrDefault(m => m.nodeType == blueprint.nodeType);
-            if (!string.IsNullOrEmpty(mapping.sceneName))
+            if (targetNode == _currentNode) 
             {
-                targetSceneName = mapping.sceneName;
+                EnterStage(_currentNode);
+                return;
+            }
+
+            if (_currentNode != null && !_currentNode.IsCleared)
+            {
+                Debug.LogWarning("현재 노드를 클리어해야 다음 노드로 이동할 수 있습니다!");
+                return;
+            }
+
+            ExpeditionPath path = _currentNode.GetPathTo(targetNode);
+            if (path != null)
+            {
+                _isMoving = true;
+                player.MoveAlongPath(path.GetCurvePoints(_currentNode.transform.position), () =>
+                {
+                    _isMoving = false;
+                    _currentNode = targetNode;
+                    
+                    _savedCurrentNodeName = _currentNode.name; 
+                    
+                    UpdateAllNodesVisuals(FindObjectsByType<ExpeditionNode>(FindObjectsSortMode.None)); 
+                    
+                    EnterStage(_currentNode);
+                });
             }
             else
             {
-                targetSceneName = "BattleScene";
-                Debug.LogWarning($"[{blueprint.nodeType}]에 매핑된 씬이 없어 기본 씬으로 이동합니다.");
+                Debug.Log($"이동 불가: [{_currentNode.name}]에서 [{targetNode.name}]로 연결된 경로가 없습니다.");
+            }
+        }
+
+        private void EnterStage(ExpeditionNode node)
+        {
+            if (node.NodeData != null && node.NodeData.nodeType == ExpeditionNodeType.Event)
+            {
+                GameObject targetUIPrefab = null;
+                EventNodeSO currentEventData = node.NodeData as EventNodeSO;
+
+                if (_canvas == null)
+                {
+                    Canvas[] canvas = FindObjectsOfType<Canvas>();
+                    foreach (var canva in canvas)
+                    {
+                        if (canva.gameObject.name == "UI")
+                        {
+                            _canvas = canva;
+                            break;
+                        }
+                    }
+                }
+
+                foreach (var mapping in eventUIMappings)
+                {
+                    if (mapping.eventNodeData == currentEventData)
+                    {
+                        targetUIPrefab = mapping.uiPanel;
+                        break;
+                    }
+                }
+
+                if (targetUIPrefab == null && eventUIMappings.Count > 0)
+                {
+                    targetUIPrefab = eventUIMappings[0].uiPanel;
+                    Debug.Log("매칭되는 EventNodeSO가 없어 기본 UI를 사용합니다.");
+                }
+
+                if (targetUIPrefab != null && _canvas != null)
+                {
+                    GameObject uiInstance = Instantiate(targetUIPrefab, _canvas.transform);
+                    uiInstance.transform.localPosition = new Vector3(13, -82, -12f);
+                    uiInstance.SetActive(true);
+                }
+                else
+                {
+                    Debug.LogWarning("이벤트 UI 프리팹 또는 Canvas를 찾을 수 없습니다.");
+                }
+
+                _isMoving = false;
+                return;
+            }
+
+            string targetSceneName = node.TargetSceneName;
+
+            if (node.NodeData is BattleNodeSO battleNodeData)
+                targetSceneName = battleNodeData.GetRandomBattleSceneName();
+
+            if (string.IsNullOrEmpty(targetSceneName))
+            {
+                Debug.LogWarning($"[{node.name}] 노드에 이동할 씬 이름이 설정되지 않았습니다.");
+                return;
             }
 
             if (SceneChangeManager.Instance != null)
@@ -228,18 +341,5 @@ namespace Code.Expedition.Managers
             else
                 SceneManager.LoadScene(targetSceneName);
         }
-
-        private void OnStageCleared(StageClearEvent evt)
-        {
-            if (evt.isClear)
-            {
-                Debug.Log("스테이지 클리어 확인 - 맵 선택 가능 상태로 변경됩니다.");
-                Locked = false;
-                SaveMap();
-            }
-        }
-
-        public void SetAttainableNodes() { if (view != null) view.SetAttainableNodes(); }
-        public void SetLineColors() { if (view != null) view.SetLineColors(); }
     }
 }

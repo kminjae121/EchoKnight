353d2990c8b3308ed14fbdd0912b9ebcd6495d58
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2026-04-25 23:31:48 +0900

    feat :UnitTrait

---
+++ Assets/_Code/KMJ/UnitSystem/Unit/UnitAttributes/UnitTrait.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Code.UnitSystem.UnitAttributes
+{
+    public class UnitTrait : MonoBehaviour, IUnitComponent
+    {
+        private Unit _unit;
+        private UnitTraitPerform perform;
+        private UnitTraitCondition condition;
+        
+        public void Initialize(Unit owner)
+        {
+            _unit = owner;
+
+            condition = GetComponentInChildren<UnitTraitCondition>();
+            
+            perform = GetComponentInChildren<UnitTraitPerform>();
+
+            if (_unit != null)
+            {
+                condition.Initialize(_unit);
+                perform.Initialize(_unit);
+            }
+            else
+                Debug.LogWarning("유닛이 할당되어있지 않습니다.");
+        }
+
+        private void CheckCondition()
+        {
+            if (condition.CheckCondition())
+            {
+                Perform();
+            }
+        }
+
+        private void Perform()
+        {
+            perform.PerformTrait();
+        }
+    }
+}
---
+++ Assets/_Code/KMJ/UnitSystem/Unit/UnitAttributes/UnitTraitPerform.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Code.UnitSystem.UnitAttributes
+{
+    public abstract class UnitTraitPerform : MonoBehaviour
+    {
+        private Unit _unit;
+
+        public void Initialize(Unit unit)
+        {
+            _unit = unit;
+        }
+
+        public virtual void PerformTrait()
+        {
+            
+        }
+    }
+}

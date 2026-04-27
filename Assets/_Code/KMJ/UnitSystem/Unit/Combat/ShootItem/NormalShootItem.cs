67c0a7f42ece9b0e22fdb1edb37a5560cab83f05
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2026-04-27 16:02:52 +0900

    feat : Shoot

--- Assets/_Code/KMJ/UnitSystem/Unit/Combat/ShootItem/NormalShootItem.cs
+++ Assets/_Code/KMJ/UnitSystem/Unit/Combat/ShootItem/NormalShootItem.cs
@@ -1,11 +1,13 @@
-﻿using Code.Core.Events.Bus;
+﻿    using Code.Core.Events.Bus;
+    using UnityEngine;
 
-namespace Code.UnitSystem.Combat
+    namespace Code.UnitSystem.Combat
 {
     public class NormalShootItem : ShootItem
     {
         public override void AttackEnd()
         {
+            Debug.Log("Attacks");
             Bus<DamageEvent>.Raise(new DamageEvent(_shootItemManager.DamageData,_target,0,_shootItemManager.Unit
                 , false,false,0.2f));
             
--- Assets/_Code/KMJ/UnitSystem/Unit/Combat/ShootItem/PiercingItem.cs
+++ Assets/_Code/KMJ/UnitSystem/Unit/Combat/ShootItem/PiercingItem.cs
@@ -16,6 +16,8 @@ namespace Code.UnitSystem.Combat
 
         private void ContinueArrow()
         {
+            Debug.Log(_target);
+            
             Bus<DamageEvent>.Raise(new DamageEvent(_shootItemManager.DamageData,_target,0,_shootItemManager.Unit
                 , false,false,0.2f));
             
